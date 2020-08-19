9283604d352a33e6d65ce2ea8d282bb69309c9fe
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-08-19 14:38:42 +0900

    - 戻るボタンがタップされると、スライドショー画面に戻り、同じ画像を表示してください - 画像は、Xcodeのナビゲーションエリア（Project Navigator）へドラッグアンドドロップし、Copy items if neededにチェックを入れて、Xcodeプロジェクトへコピーしてください を修正。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
     
@@ -22,19 +22,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     
     @IBOutlet weak var switchButton: UIButton!
-   
-// 配列に指定するindex番号を宣言
+    
+    // 配列に指定するindex番号を宣言
     var nowIndex:Int = 0
-//スライドショーに使用するタイマーを宣言
+    //スライドショーに使用するタイマーを宣言
     var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
-//初期画面
-//バンドルした画像ファイルを読み込み
-//Image Viewに画像を設定
+        
+        //初期画面
+        //バンドルした画像ファイルを読み込み
+        //Image Viewに画像を設定
         let selectedImage = UIImage(named: "cat1")
         imageView.image = selectedImage
     }
@@ -43,8 +43,8 @@ class ViewController: UIViewController {
     var displayImageNo = 0
     
     // 画像の名前の配列
-   var imageNameArray = ["cat1", "cat2", "cat3", "cat4", "cat5"]
-
+    var imageNameArray = ["cat1", "cat2", "cat3", "cat4", "cat5"]
+    
     // 表示している画像の番号を元に画像を表示する
     func displayImage() {
         
@@ -57,25 +57,25 @@ class ViewController: UIViewController {
         // Image Viewに読み込んだ画像をセット
         imageView.image = selectedImage
     }
-
+    
     @IBAction func nextButtonTap(_ sender: Any) {
         if displayImageNo < imageNameArray.count - 1 {
-// 表示している画像の番号を１増やす
+            // 表示している画像の番号を１増やす
             displayImageNo += 1
-//表示している画像の番号をもとに画像を表示する
+            //表示している画像の番号をもとに画像を表示する
             displayImage()
         } else {
             displayImageNo = 0
             displayImage()
         }
-       
+        
     }
     
     @IBAction func backButtonTap(_ sender: Any) {
         if displayImageNo >= 1 && displayImageNo <= imageNameArray.count - 1 {
-// 表示している画像の番号を１減らす
+            // 表示している画像の番号を１減らす
             displayImageNo -= 1
-//表示している画像の番号をもとに画像を表示する
+            //表示している画像の番号をもとに画像を表示する
             displayImage()
         } else {
             displayImageNo = imageNameArray.count - 1
@@ -87,59 +87,69 @@ class ViewController: UIViewController {
     @IBAction func switchButtonTap(_ sender: Any) {
         backButton.isEnabled = false
         nextButton.isEnabled = false
-//再生中か停止しているかを判定
+        //再生中か停止しているかを判定
         if (timer == nil){
             
-//タイマーをセットする
-timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
-//ボタンの名前を停止に変える
+            //タイマーをセットする
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            //ボタンの名前を停止に変える
             switchButton.setTitle("停止", for: .normal)
             
         } else {
-// 停止時の処理を実装
+            // 停止時の処理を実装
             backButton.isEnabled = true
             nextButton.isEnabled = true
-// タイマーを停止する
+            // タイマーを停止する
             timer.invalidate()
             
-//タイマーを削除しておく
+            //タイマーを削除しておく
             timer = nil
-//ボタンの名前を再生にしておく
+            //ボタンの名前を再生にしておく
             switchButton.setTitle("再生", for: .normal)
-         
+            
         }
-      
+        
     }
     
     @objc func changeImage(){
-         if displayImageNo < imageNameArray.count - 1 {
-        // 表示している画像の番号を１増やす
-                    displayImageNo += 1
-        //表示している画像の番号をもとに画像を表示する
-                    displayImage()
-                } else {
-                    displayImageNo = 0
-                    displayImage()
-                }
+        if displayImageNo < imageNameArray.count - 1 {
+            // 表示している画像の番号を１増やす
+            displayImageNo += 1
+            //表示している画像の番号をもとに画像を表示する
+            displayImage()
+        } else {
+            displayImageNo = 0
+            displayImage()
+        }
+      //偏移したときにタイマーを止め、同じ画像が出るようにする
+      backButton.isEnabled = true
+      nextButton.isEnabled = true
+      // タイマーを停止する
+      timer.invalidate()
+      
+      //タイマーを削除しておく
+      timer = nil
+      //ボタンの名前を再生にしておく
+      switchButton.setTitle("再生", for: .normal)
+    }
     
-}
     func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
-           // [indexPath.row] から画像名を探し、UImage を設定
+        // [indexPath.row] から画像名を探し、UImage を設定
         selectedImage = UIImage(named: imageNameArray[indexPath.row] )
-           if selectedImage != nil {
-               // SubViewController へ遷移するために Segue を呼び出す
-               performSegue(withIdentifier: "toSecondViewController",sender: nil)
-           }
-       }
+        if selectedImage != nil {
+            // SubViewController へ遷移するために Segue を呼び出す
+            performSegue(withIdentifier: "toSecondViewController",sender: nil)
+        }
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let secondViewController = segue.destination as! SecondViewController
         
-            
-        secondViewController.catImage = imageNameArray[displayImageNo]
-}
-        }
         
-       
+        secondViewController.catImage = imageNameArray[displayImageNo]
+    }
+}
+
+
 
